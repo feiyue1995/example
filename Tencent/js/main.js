0fc13df97460c23e560f52a6f87301d706a4a61f
feiyue1995 <792521822@qq.com>
2017-06-26 20:24:33 +0800

    Study
    Copy Tencent 3

---
+++ Tencent/js/main.js
@@ -0,0 +1,24 @@
+function mOver () {
+	var myDiv = document.getElementById("vista_title");
+
+	myDiv.style.display = "block";
+}
+
+function mOut () {
+	var myDiv = document.getElementById("vista_title");
+
+	myDiv.style.display = "none";
+}
+
+
+function wOver () {
+	var myDiv = document.getElementById("weichat");
+
+	myDiv.style.display = "block";
+}
+
+function wOut () {
+	var myDiv = document.getElementById("weichat");
+
+	myDiv.style.display = "none";
+}
