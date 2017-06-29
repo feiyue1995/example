4118375b22d9faa8adeefe20afedb7e4488fd03e
feiyue1995 <792521822@qq.com>
2017-06-30 01:11:37 +0800

    Study
    copy Tencent 4

--- Tencent/js/main.js
+++ Tencent/js/main.js
@@ -22,3 +22,49 @@ function wOut () {
 
 	myDiv.style.display = "none";
 }
+
+function show_1() {
+	var myDiv = document.getElementById("news_list1");
+	var myDiv2 = document.getElementById("news_list2");
+	var myDiv3 = document.getElementById("news_list3");
+
+	myDiv.style.display = "block";
+	myDiv2.style.display = "none";
+	myDiv3.style.display = "none";
+}
+
+function show_2() {
+	var myDiv = document.getElementById("news_list1");
+	var myDiv2 = document.getElementById("news_list2");
+	var myDiv3 = document.getElementById("news_list3");
+
+	myDiv.style.display = "none";
+	myDiv2.style.display = "block";
+	myDiv3.style.display = "none";
+}
+
+function show_3() {
+	var myDiv = document.getElementById("news_list1");
+	var myDiv2 = document.getElementById("news_list2");
+	var myDiv3 = document.getElementById("news_list3");
+
+	myDiv.style.display = "none";
+	myDiv2.style.display = "none";
+	myDiv3.style.display = "block";
+}
+
+function show_video_1() {
+	var myDiv = document.getElementById("text1");
+	var myDiv2 = document.getElementById("text2");
+
+	myDiv.style.display = "block";
+	myDiv2.style.display = "none";
+}
+
+function show_video_2() {
+	var myDiv = document.getElementById("text1");
+	var myDiv2 = document.getElementById("text2");
+
+	myDiv.style.display = "none";
+	myDiv2.style.display = "block";
+}
